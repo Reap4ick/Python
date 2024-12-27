1e1b89fd0b2ca7b3e55d3f14c8e77319f9cd214b
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2024-12-27 10:46:38 +0200

    new

--- djangoproject/products/urls.py
+++ djangoproject/products/urls.py
@@ -5,5 +5,9 @@ from products import views
 urlpatterns = [
     path("home/", views.home),
     path("list/", views.list),
-    path("details/<int:id>", views.details),
+    path("create/", views.create),
+    path("edit/<int:id>", views.edit, name="edit"),
+    path("details/<int:id>", views.details, name="details"),
+    path("delete/<int:id>", views.delete, name="delete"),
+
 ]
